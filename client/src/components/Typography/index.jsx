5650e65fd682742047285d179c7e2a2e9e8e22f6
Vakha Irzakhanov <47324041+irzakhanov@users.noreply.github.com>
2021-11-03 15:27:45 +0300

    Сделан компонент Typography (#21)
    * Typography version one
    
    Co-authored-by: Vakha <gvardee95@gmail.com>

---
+++ client/src/components/Typography/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import PropTypes from 'prop-types';
+
+const StyledTypography = styled.div`
+  color: ${(props) => props.theme.colors[props.color].main};
+  ${(props) => {
+    if (props.variant === 'header') {
+      return css`
+        font-size: 24px;
+        font-weight: 500;
+      `;
+    }
+    if (props.variant === 'caption') {
+      return css`
+        font-size: 16px;
+        font-weight: 500;
+      `;
+    }
+    if (props.variant === 'small') {
+      return css`
+        font-size: 12px;
+        font-weight: 400;
+      `;
+    }
+    if (props.variant === 'extraSmall') {
+      return css`
+        font-size: 12px;
+        font-weight: 400;
+      `;
+    }
+  }};
+`;
+
+export const Typography = ({ variant, children, ...props }) => {
+  return (
+    <StyledTypography {...props} variant={variant}>
+      {children}
+    </StyledTypography>
+  );
+};
+
+Typography.propTypes = {
+  children: PropTypes.string.isRequired,
+  color: PropTypes.oneOf(['danger', 'gray', 'warning', 'success']),
+  variant: PropTypes.oneOf(['header', 'caption', 'small', 'extraSmall']),
+};
+
+Typography.defaultProps = {
+  color: 'success',
+  variant: 'caption',
+};
