fb5df6408639b95cd87805d8391c583f8ad4180b
Vakha Irzakhanov <47324041+irzakhanov@users.noreply.github.com>
2021-11-03 16:59:12 +0300

    Сделал компонент Rate (#23)
    * Сделал компонент Rate
    
    Co-authored-by: Vakha <gvardee95@gmail.com>

---
+++ client/src/stories/Rate.stories.jsx
@@ -0,0 +1,35 @@
+import { Rate } from '../components/Rate';
+
+export default {
+  title: 'Example/Rate',
+  component: Rate,
+};
+
+const Template = (args) => <Rate {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  currentRate: 0,
+};
+
+export const VariantPlus = Template.bind({});
+VariantPlus.args = {
+  currentRate: 67,
+};
+
+export const VariantMinus = Template.bind({});
+VariantMinus.args = {
+  currentRate: -41,
+};
+
+export const VariantUpArrow = Template.bind({});
+VariantUpArrow.args = {
+  currentRate: 7,
+  isUpped: true,
+};
+
+export const VariantDownArrow = Template.bind({});
+VariantDownArrow.args = {
+  currentRate: 579,
+  isDowned: true,
+};
