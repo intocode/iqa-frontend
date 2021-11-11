b1cfc75fb1223f3d6c6d801966d74f2acdb2b578
Vakha Irzakhanov <47324041+irzakhanov@users.noreply.github.com>
2021-11-11 11:23:22 +0300

    Сделал слайс для тэгов (#14)
    * Сделал слайс для тэгов
    
    * Доработал слайс для тэгов
    
    Co-authored-by: Vakha <gvardee95@gmail.com>

---
+++ src/features/tags/tagsSlice.js
@@ -0,0 +1,81 @@
+import {
+  createSlice,
+  createAsyncThunk,
+  createSelector,
+} from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchTags = createAsyncThunk('tags/fetch', async (_, thunkAPI) => {
+  try {
+    const response = await axios.get(`/tags`);
+
+    return response.data;
+  } catch (e) {
+    return thunkAPI.rejectWithValue(e.message);
+  }
+});
+
+export const fetchTagsByQuery = createAsyncThunk(
+  'tags/fetchByQuery',
+  async (query, thunkAPI) => {
+    try {
+      const response = await axios.get(`/tags?q=${query}`);
+
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addTag = createAsyncThunk('tags/add', async (data, thunkAPI) => {
+  try {
+    const response = await axios.post(`/tags`, data);
+
+    return response.data;
+  } catch (e) {
+    return thunkAPI.rejectWithValue(e.message);
+  }
+});
+
+const tagsSlice = createSlice({
+  name: 'tags',
+  initialState: {
+    tags: [],
+    loading: false,
+    error: '',
+  },
+  extraReducers: {
+    [fetchTagsByQuery.pending]: (state) => {
+      state.loading = true;
+    },
+    [fetchTagsByQuery.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.tags = action.payload;
+    },
+    [addTag.pending]: (state) => {
+      state.loading = true;
+    },
+    [addTag.fulfilled]: (state) => {
+      state.loading = false;
+    },
+    [addTag.rejected]: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+  },
+});
+
+const selectTagsState = (state) => state.tags;
+
+export const selectTags = createSelector(
+  selectTagsState,
+  (state) => state.tags
+);
+
+export const selectTagsError = createSelector(
+  selectTagsState,
+  (state) => state.error
+);
+
+export default tagsSlice.reducer;
