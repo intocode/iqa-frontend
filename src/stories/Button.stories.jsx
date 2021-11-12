24efb074de898ce13340f1ccfb40e7ee99691591
Muslim Tsokaev <47528621+katada93@users.noreply.github.com>
2021-11-12 19:23:43 +0300

    Фиксы для историй Button и Alert (#27)
    - Добавил все доступные цвета для историй `Button`
    - Убрал `args` в истории `Alert`

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 import { Button } from '../components/Button';
 
 export default {
@@ -8,23 +9,96 @@ export default {
   },
 };
 
-const Template = (args) => <Button {...args}>Click me</Button>;
+const StyledDecorator = styled.div`
+  & > button {
+    margin-right: 15px;
+  }
+`;
 
-export const Variants = Template.bind({});
-Variants.args = {};
+export const Variants = () => (
+  <StyledDecorator>
+    <Button contrast={false}>Click me</Button>
+    <Button contrast={false} color="success">
+      Click me
+    </Button>
+    <Button contrast={false} color="gray">
+      Click me
+    </Button>
+    <Button contrast={false} color="danger">
+      Click me
+    </Button>
+    <Button contrast={false} color="warning">
+      Click me
+    </Button>
+  </StyledDecorator>
+);
 
-export const Disabled = Template.bind({});
-Disabled.args = {
-  disabled: true,
-  contrast: false,
-};
+export const Contrasted = () => (
+  <StyledDecorator>
+    <Button>Click me</Button>
+    <Button color="success">Click me</Button>
+    <Button color="gray">Click me</Button>
+    <Button color="danger">Click me</Button>
+    <Button color="warning">Click me</Button>
+  </StyledDecorator>
+);
 
-export const Preloader = Template.bind({});
-Preloader.args = {
-  loading: true,
-};
+export const Disabled = () => (
+  <StyledDecorator>
+    <Button contrast={false} disabled>
+      Click me
+    </Button>
+    <Button contrast={false} disabled color="success">
+      Click me
+    </Button>
+    <Button contrast={false} disabled color="gray">
+      Click me
+    </Button>
+    <Button contrast={false} disabled color="danger">
+      Click me
+    </Button>
+    <Button contrast={false} disabled color="warning">
+      Click me
+    </Button>
+  </StyledDecorator>
+);
 
-export const Rounded = Template.bind({});
-Rounded.args = {
-  rounded: true,
-};
+export const Preloader = () => (
+  <StyledDecorator>
+    <Button contrast={false} loading>
+      Click me
+    </Button>
+    <Button contrast={false} loading color="success">
+      Click me
+    </Button>
+    <Button contrast={false} loading color="gray">
+      Click me
+    </Button>
+    <Button contrast={false} loading color="danger">
+      Click me
+    </Button>
+    <Button contrast={false} loading color="warning">
+      Click me
+    </Button>
+  </StyledDecorator>
+);
+
+export const Rounded = () => (
+  <StyledDecorator>
+    <Button contrast={false} rounded>
+      Click me
+    </Button>
+    <Button contrast={false} rounded color="success">
+      Click me
+    </Button>
+    <Button contrast={false} rounded color="gray">
+      Click me
+    </Button>
+    <Button contrast={false} rounded color="danger">
+      Click me
+    </Button>
+    <Button contrast={false} rounded color="warning">
+      Click me
+    </Button>
+  </StyledDecorator>
+);
