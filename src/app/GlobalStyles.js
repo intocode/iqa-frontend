5ac287692dc2b9b50e751d292733e7a37ab37720
kuduzow <ammya@ya.ru>
2021-11-13 13:13:02 +0300

    Глобальные стили вынесены в отдельный файл

---
+++ src/app/GlobalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+* {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.container {
+  max-width: 844px;
+}
+`;
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import { Provider as ReduxProvider } from 'react-redux';
 import axios from 'axios';
 import App from './app/App';
@@ -9,25 +9,19 @@ import { theme } from './app/theme';
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import { store } from './app/store';
 import { BASE_API_URL, LS_TOKEN_KEY } from './common/constants';
+import { GlobalStyles } from './app/GlobalStyles';
 
 axios.defaults.baseURL = BASE_API_URL;
 axios.defaults.headers.authorization = `Bearer ${localStorage.getItem(
   LS_TOKEN_KEY
 )}`;
 
-const Global = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-    margin: 0;
-  }
-`;
-
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <ReduxProvider store={store}>
         <AuthProvider>
-          <Global />
+          <GlobalStyles />
           <App />
         </AuthProvider>
       </ReduxProvider>
