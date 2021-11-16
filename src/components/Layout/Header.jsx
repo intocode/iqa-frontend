ffab7d68a4e0f0b12390ed614660bdfbbe4584ae
danilbek <deni200040@gmail.com>
2021-11-16 22:59:14 +0300

    Сделал компонент Header

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,7 +1,8 @@
 import { useAuth } from '../../common/context/Auth/useAuth';
+import { Badge } from '../ui/Badge';
 import { Button } from '../ui/Button';
-
-// черновой вариант хидера
+import { Typography } from '../ui/Typography';
+import { Logo } from './Logo';
 
 export const Header = () => {
   const { token, executeLoggingInProcess, logout } = useAuth();
@@ -9,12 +10,36 @@ export const Header = () => {
     <div style={{ backgroundColor: 'white' }}>
       <div className="container mb-2 py-2">
         <div className="row align-items-center">
-          <div className="col">iqa</div>
+          <div className="col d-flex align-items-center">
+            <div className="me-3">
+              <Logo />
+            </div>
+            <Typography>Главная</Typography>
+            <Badge content={5}>
+              <Typography>Избранные</Typography>
+            </Badge>
+          </div>
           <div className="col-auto">
             {token ? (
-              <Button onClick={logout}>Выйти</Button>
+              <>
+                <Button
+                  className="me-3"
+                  contrast={false}
+                  color="primary"
+                  onClick={logout}
+                >
+                  Добавить вопрос
+                </Button>
+                <Button contrast={false} color="primary" onClick={logout}>
+                  Выйти
+                </Button>
+              </>
             ) : (
-              <Button onClick={executeLoggingInProcess}>
+              <Button
+                contrast={false}
+                color="primary"
+                onClick={executeLoggingInProcess}
+              >
                 Login with GitHub
               </Button>
             )}
