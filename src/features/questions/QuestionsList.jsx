14e75a56e8f0f534e3bd807019b2f083bfd444d7
Muslim <tsokaev93@mail.ru>
2021-11-26 18:17:32 +0300

    Добавил заголовок на главной странице

--- src/features/questions/QuestionsList.jsx
+++ src/features/questions/QuestionsList.jsx
@@ -1,7 +1,26 @@
+import styled from 'styled-components';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchQuestions, selectQuestions } from './questionsSlice';
 import { QuestionBlock } from './QuestionBlock';
+import { Typography } from '../../components/ui/Typography';
+
+const StyledQuestionList = styled.div`
+  & .top {
+    margin: 20px 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    & h2 {
+      font-weight: 500;
+      font-size: 22px;
+      line-height: 22px;
+    }
+    & div {
+      font-size: 14px;
+    }
+  }
+`;
 
 export const QuestionsList = () => {
   const dispatch = useDispatch();
@@ -11,7 +30,11 @@ export const QuestionsList = () => {
   useEffect(() => dispatch(fetchQuestions()), [dispatch]);
 
   return (
-    <div className="container">
+    <StyledQuestionList className="container">
+      <div className="top">
+        <h2>Все вопросы</h2>
+        <Typography>Показать все вопросы</Typography>
+      </div>
       {questions.map((question) => (
         <QuestionBlock
           key={question._id}
@@ -24,6 +47,6 @@ export const QuestionsList = () => {
           user={question.user}
         />
       ))}
-    </div>
+    </StyledQuestionList>
   );
 };
