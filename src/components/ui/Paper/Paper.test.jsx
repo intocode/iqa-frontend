247f8600da2d7e7b66efb5744b3b617319f1918c
Baisangur <idigov-98@mail.ru>
2021-12-13 20:50:28 +0300

    Тесты для Paper

---
+++ src/components/ui/Paper/Paper.test.jsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import { Paper } from '.';
+import '@testing-library/jest-dom';
+
+const children = 'lorem ipsum'
+
+const PaperTest = (props) => <Paper {...props}/>;
+
+describe('Paper', () => {
+
+  it('should render the Paper', () => {
+    const component = render(<PaperTest>{children}</PaperTest>)
+    const { tagName } = component.container
+    expect(tagName.toLowerCase()).toEqual("div")
+  });
+
+  it('should render the Paper component', () => {
+    render(<PaperTest>{children}</PaperTest>)
+    expect(screen.getByText("lorem ipsum")).toBeInTheDocument();
+  });
+
+});
