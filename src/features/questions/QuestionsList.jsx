67c7406ff640f9625d0b895330054284ed7f3b10
Adam <bisliev_adam@mail.ru>
2022-01-05 16:22:22 +0300

    Сделал переключатель switch

--- src/features/questions/QuestionsList.jsx
+++ src/features/questions/QuestionsList.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   fetchQuestions,
@@ -8,12 +8,15 @@ import {
 import { QuestionBlock } from './QuestionBlock';
 import { QuestionsListPlaceholder } from './QuestionsListPlaceholder';
 import { Title } from '../../app/Title/Title';
+import { Switch } from '../../components/ui/Switch';
 
 export const QuestionsList = () => {
   const dispatch = useDispatch();
 
   const questions = useSelector(selectQuestions);
   const loading = useSelector(selectQuestionsLoading);
+
+  const [isCompactMode, setIsCompactMode] = useState(false);
 
   useEffect(() => dispatch(fetchQuestions()), [dispatch]);
 
@@ -26,6 +29,15 @@ export const QuestionsList = () => {
         <div className="row justify-content-between align-items-center my-3">
           <div className="col">
             <h2>Все вопросы</h2>
+          </div>
+          <div className="col-auto">
+            <Switch
+              on={isCompactMode}
+              onChange={() => setIsCompactMode(!isCompactMode)}
+              disabled={false}
+            >
+              Компактный вид
+            </Switch>
           </div>
         </div>
         {loading && <QuestionsListPlaceholder />}
