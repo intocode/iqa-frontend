3d1ca0d98dd4d70feabccbb8ec386aa96905d504
Khadziev <82869463+Khadziev@users.noreply.github.com>
2022-01-21 15:49:49 +0300

    Все иконки из crs/components/assets преобразовал в  компоненты (#149)

---
+++ src/components/icons/PlusIcon.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function PlusIcon() {
+  return (
+    <div>
+      <svg
+        width="12"
+        height="12"
+        viewBox="0 0 12 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M6 2V10" stroke="#606266" strokelinejoin="round" />
+        <path d="M2 6H10" stroke="#606266" strokelinejoin="round" />
+      </svg>
+    </div>
+  );
+}
+
+export default PlusIcon;
