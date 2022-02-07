29df89178272cc3ab1a87f2381a49f464e29ad73
Satuev <alitm.s@mail.ru>
2022-02-07 14:02:21 +0300

    Добавил спиннер при добавлении в избранное

--- src/features/questions/QuestionBlock.jsx
+++ src/features/questions/QuestionBlock.jsx
@@ -147,6 +147,42 @@ const StyledDeleteGroup = styled.div`
   margin-left: 20px;
 `;
 
+const StyledLoading = styled.div`
+  display: inline-block;
+  margin-right: 8px;
+  position: relative;
+  width: 11px;
+  height: 11px;
+  & > div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 14px;
+    height: 14px;
+    border: 2px solid #e6a23c;
+    border-radius: 50%;
+    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: #e6a23c transparent transparent transparent;
+  }
+  & > div:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+  & > div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+  & > div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+  @keyframes lds-ring {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
 export const QuestionBlock = ({ question, isCompactMode }) => {
   const { token } = useAuth();
   const user = useSelector(selectProfile);
@@ -234,7 +270,16 @@ export const QuestionBlock = ({ question, isCompactMode }) => {
           </StyledAction>
           {token && (
             <StyledAction onClick={handleToggleFavorite}>
-              <img src={iconFavorites} alt="" />
+              {addingToFavorites || deletingFromFavorites ? (
+                <StyledLoading>
+                  <div />
+                  <div />
+                  <div />
+                  <div />
+                </StyledLoading>
+              ) : (
+                <img src={iconFavorites} alt="" />
+              )}
               <StyledFavorites className="d-none d-md-block">
                 {questionByFavorites ? deletingStatus : addingStatus}
               </StyledFavorites>
