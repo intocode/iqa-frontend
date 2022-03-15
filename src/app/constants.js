f833e18ed6eba8b7749c1c9322a109036c3a06d3
Hamza <82868450+tepsurkaev@users.noreply.github.com>
2022-03-15 15:52:38 +0300

    Сделал константу для отоброжения scroll-to-top (#203)

--- src/app/constants.js
+++ src/app/constants.js
@@ -2,3 +2,4 @@ import { theme } from './theme';
 
 export const AVAILABLE_THEME_COLORS = Object.keys(theme.colors);
 export const DEFAULT_COLOR = theme.defaultColor;
+export const SCROLL_TO_TOP_SHOW = 500;
