5008d7b35faa65c01b833959091f985c528cfac1
Kuduzow Akhmad <ammya@ya.ru>
2022-04-23 16:17:01 +0300

    Установлен новый основной бекенд сервер (#212)

--- src/common/utils/url.js
+++ src/common/utils/url.js
@@ -2,7 +2,7 @@ export const getBaseUrl = () => {
   const PRODUCTION = process.env.NODE_ENV === 'production';
   const STAGING = !!process.env.REACT_APP_STAGING;
 
-  let baseUrl = 'https://iqa-be.herokuapp.com/';
+  let baseUrl = 'https://iqa-backend.intocode.ru/';
 
   if (!PRODUCTION || STAGING) {
     baseUrl = 'https://iqa-stage-backend.intocode.ru/';
