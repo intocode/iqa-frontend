0460ca082ca28661c1710fbea5975e13ebd5f709
kuduzow <admin@intocode.ru>
2022-06-06 18:31:11 +0300

    Обновлен адрес сервера

--- src/common/utils/url.js
+++ src/common/utils/url.js
@@ -2,7 +2,7 @@ export const getBaseUrl = () => {
   const PRODUCTION = process.env.NODE_ENV === 'production';
   const STAGING = !!process.env.REACT_APP_STAGING;
 
-  let baseUrl = 'https://iqa-backend.intocode.ru/';
+  let baseUrl = 'https://iqa-server.intocode.ru/';
 
   if (!PRODUCTION || STAGING) {
     baseUrl = 'https://iqa-stage-backend.intocode.ru/';
