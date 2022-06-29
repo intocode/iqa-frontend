7fa27a4b969d5553da097428e0e93204db20e98c
Magomed Akhmadov <89927956+Husseyn99@users.noreply.github.com>
2022-06-29 12:41:45 +0300

    Добавил количество просмотров к вопросам (#239)
    * Добавил количество просмотров к вопросам
    
    * Внес корректировки в код
    
    * Добавил классы из бутстрапа

--- src/features/questions/questions-list/question-actions/QuestionsActions.jsx
+++ src/features/questions/questions-list/question-actions/QuestionsActions.jsx
@@ -4,12 +4,14 @@ import PropTypes from 'prop-types';
 import { FavoriteAction } from './FavoriteAction';
 import { CommentsAction } from './CommentsAction';
 import { DeleteAction } from './DeleteAction';
+import QuestionViews from '../QuestionViews';
 
 export const QuestionsActions = ({ questionId }) => {
   return (
     <div className="row justify-content-end justify-content-md-start">
       <FavoriteAction questionId={questionId} />
       <CommentsAction questionId={questionId} />
+      <QuestionViews questionId={questionId} />
       <DeleteAction questionId={questionId} />
     </div>
   );
