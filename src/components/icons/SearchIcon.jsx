352ed0138ea86d4210dc2a9c38df743dea7399f7
Husein <ahmadov.m48@gmail.com>
2022-06-29 14:15:51 +0300

    Убрал анимацию для поискового поля

--- src/components/icons/SearchIcon.jsx
+++ src/components/icons/SearchIcon.jsx
@@ -12,10 +12,10 @@ function SearchIcon() {
       >
         <path
           d="M6.41667 11.0833C8.994 11.0833 11.0833 8.994 11.0833 6.41667C11.0833 3.83934 8.994 1.75 6.41667 1.75C3.83934 1.75 1.75 3.83934 1.75 6.41667C1.75 8.994 3.83934 11.0833 6.41667 11.0833Z"
-          stroke="#49a2fe"
+          stroke="#000000"
           strokeLinejoin="round"
         />
-        <path d="M12.85 12.85L9.71249 9.71252" stroke="#49a2fe" strokeLinejoin="round" />
+        <path d="M12.85 12.85L9.71249 9.71252" stroke="#000000" strokeLinejoin="round" />
       </svg>
     </div>
   );
