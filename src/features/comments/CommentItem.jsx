1331eadd80706c908aa241ae219c3aa5b22bdd4d
SaydAliUsmanov <usmanov.guno@gmail.com>
2022-07-29 23:00:41 +0300

    Реализовал фичу удаления комментария вопроса

--- src/features/comments/CommentItem.jsx
+++ src/features/comments/CommentItem.jsx
@@ -1,11 +1,22 @@
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
 import { commentsSelectors } from './commentsSlice';
 import { CommentView } from './CommentView';
 
+const StyledCommentBlock = styled.div`
+  &:hover .delete {
+    opacity: 1;
+  }
+`;
+
 export const CommentItem = ({ commentId }) => {
   const comment = useSelector((state) => commentsSelectors.selectById(state, commentId));
-  return <CommentView comment={comment} />;
+  return (
+    <StyledCommentBlock>
+      <CommentView comment={comment} />
+    </StyledCommentBlock>
+  );
 };
 
 CommentItem.propTypes = {
