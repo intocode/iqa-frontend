54baeb7f430e6ed87d4610444cd3d30c8bef8fee
Adam <98719246+baysarov077@users.noreply.github.com>
2022-09-11 00:06:21 +0300

    Заменил компонент Alert (#273)

--- src/components/ui/index.jsx
+++ src/components/ui/index.jsx
@@ -1,8 +1,7 @@
-import { Alert } from './Alert';
 import { Badge } from './Badge';
 import { Input } from './Input';
 import { Paper } from './Paper';
 import { Typography } from './Typography';
 import { Switch } from './Switch';
 
-export { Alert, Badge, Input, Paper, Typography, Switch };
+export { Badge, Input, Paper, Typography, Switch };
