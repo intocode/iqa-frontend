b8361926071e714092c237a7e32cf2da0f989eb6
Adam <adambajsarov@gmail.com>
2022-09-11 00:40:04 +0300

    Заменил компонент Input

--- src/components/ui/index.jsx
+++ src/components/ui/index.jsx
@@ -1,6 +1,5 @@
-import { Input } from './Input';
 import { Paper } from './Paper';
 import { Typography } from './Typography';
 import { Switch } from './Switch';
 
-export { Input, Paper, Typography, Switch };
+export { Paper, Typography, Switch };
