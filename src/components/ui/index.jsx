1ad91ae68124f0c166cae9517caced8256d3ffeb
Adam <98719246+baysarov077@users.noreply.github.com>
2022-09-11 00:27:11 +0300

    Удалил компонент Badge (#274)

--- src/components/ui/index.jsx
+++ src/components/ui/index.jsx
@@ -1,7 +1,6 @@
-import { Badge } from './Badge';
 import { Input } from './Input';
 import { Paper } from './Paper';
 import { Typography } from './Typography';
 import { Switch } from './Switch';
 
-export { Badge, Input, Paper, Typography, Switch };
+export { Input, Paper, Typography, Switch };
