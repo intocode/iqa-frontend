0911e8c5dafe87db2ae8396418706b358781262b
Adam <98719246+baysarov077@users.noreply.github.com>
2022-09-16 16:16:37 +0300

    Заменил компонент Popover (#279)

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,22 +1,19 @@
 import { Link } from 'react-router-dom';
 import { useEffect, useState, useRef } from 'react';
 import styled from 'styled-components';
-import { useDispatch, useSelector } from 'react-redux';
-import { Button, Divider, Typography } from 'antd';
+import { useSelector } from 'react-redux';
+import { Button, Popover } from 'antd';
 import { useAuth } from '../../../common/context/Auth/useAuth';
 import { Logo } from './Logo';
 import AdaptiveMenu from './AdaptiveMenu';
 import MenuIcon from '../../icons/MenuIcon';
 import CloseMenuIcon from '../../icons/CloseMenuIcon';
-import { resetProfile, selectProfile } from '../../../features/profile/profileSlice';
-import Popover from '../../ui/Popover';
-import { Paper } from '../../ui';
-import { LinkToFavorites } from './header-menu/LinkToFavorites';
-import { LinkToDeleted } from './header-menu/LinkToDeleted';
+import { selectProfile } from '../../../features/profile/profileSlice';
+// import { Paper } from '../../ui';
 import Search from './Search';
 import ArrowAvatar from '../../icons/ArrowAvatar';
-import LinkToProfilePage from './header-menu/LinkToProfilePage';
 import HelpIcon from '../../icons/HelpIcon';
+import PopoverContent from './PopoverContent';
 
 const StyledHeader = styled.div`
   box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
@@ -63,41 +60,16 @@ const StyledAvatar = styled.div`
   }
 `;
 
-const StyledMenuProfile = styled.div`
-  line-height: 1.7;
-  margin: 10px 0;
-`;
-
-const StyledMenuList = styled.ul`
-  list-style: none;
-  padding: 0;
-`;
-
-const StyledWrapperPaper = styled.div`
-  margin-top: 5px;
-  & > div {
-    font-size: 14px;
-    color: #909399;
-  }
-  .fullName {
-    font-size: 16px;
-    color: black;
-    padding-bottom: 5px;
-  }
-`;
-
 export const Header = () => {
   // todo: рефакторить мобильную версию. Возможно нужен вынос в хук или в контекст
 
-  const { token, executeLoggingInProcess, logout } = useAuth();
+  const { token, executeLoggingInProcess } = useAuth();
 
   const [mobileMenu, setMobileMenu] = useState(false);
 
   const [openMenuProfile, setOpenMenuProfile] = useState(false);
 
   const ref = useRef();
-
-  const dispatch = useDispatch();
 
   // todo: зачем это нужно?
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -129,14 +101,11 @@ export const Header = () => {
     setOpenMenuProfile(!openMenuProfile);
   };
 
-  const handleClick = () => {
-    dispatch(resetProfile());
-    logout();
-  };
   // todo: рефакторить
   const iconMenuAndClose = !mobileMenu ? <MenuIcon /> : <CloseMenuIcon />;
 
   const { REACT_APP_FEATURE_ADD_QUESTION } = process.env;
+
   return (
     <StyledHeader>
       <AdaptiveMenu toggleMobileMenu={handleToggleMenu} mobileMenu={mobileMenu} />
@@ -170,47 +139,14 @@ export const Header = () => {
                   </Link>
                 </div>
                 <div>
-                  <StyledAvatar onClick={handleOpenMenuProfile} ref={ref} className="d-md-flex">
-                    <img className="m-auto" src={profile.avatar?.thumbnail} alt="" />
-                    <div className={openMenuProfile ? 'downArrow' : 'upArrow'}>
-                      <ArrowAvatar />
-                    </div>
-                  </StyledAvatar>
-                  {openMenuProfile && (
-                    <Popover
-                      open={openMenuProfile}
-                      onClose={setOpenMenuProfile}
-                      anchorEl={ref}
-                      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                    >
-                      <StyledWrapperPaper>
-                        <Paper className="d-flex flex-column">
-                          <div className="fullName">{profile.fullName}</div>
-                          <div>@{profile.name}</div>
-                          <div role="presentation" onClick={handleOpenMenuProfile}>
-                            <StyledMenuProfile>
-                              <Divider className="m-0" />
-                              <StyledMenuList>
-                                <li className="mt-2">
-                                  <LinkToProfilePage />
-                                </li>
-                                <li>
-                                  <LinkToFavorites />
-                                </li>
-                                <li className="mb-2">
-                                  <LinkToDeleted />
-                                </li>
-                              </StyledMenuList>
-                              <Divider className="m-0" />
-                            </StyledMenuProfile>
-                            <Link to="/" className="d-none d-md-block">
-                              <Typography.Link onClick={handleClick}>Выйти</Typography.Link>
-                            </Link>
-                          </div>
-                        </Paper>
-                      </StyledWrapperPaper>
-                    </Popover>
-                  )}
+                  <Popover placement="bottom" trigger="click" content={PopoverContent}>
+                    <StyledAvatar onClick={handleOpenMenuProfile} ref={ref} className="d-md-flex">
+                      <img className="m-auto" src={profile.avatar?.thumbnail} alt="" />
+                      <div className={openMenuProfile ? 'downArrow' : 'upArrow'}>
+                        <ArrowAvatar />
+                      </div>
+                    </StyledAvatar>
+                  </Popover>
                 </div>
               </>
             ) : (
---
+++ src/components/layout/header/PopoverContent.jsx
@@ -0,0 +1,61 @@
+import { Divider, Typography } from 'antd';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { resetProfile, selectProfile } from '../../../features/profile/profileSlice';
+import { LinkToDeleted } from './header-menu/LinkToDeleted';
+import { LinkToFavorites } from './header-menu/LinkToFavorites';
+import LinkToProfilePage from './header-menu/LinkToProfilePage';
+import { useAuth } from '../../../common/context/Auth/useAuth';
+
+const StyledMenuProfile = styled.div`
+  line-height: 1.7;
+  margin: 10px 0;
+`;
+
+const StyledMenuList = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+const PopoverContent = () => {
+  const { logout } = useAuth();
+
+  const dispatch = useDispatch();
+
+  const profile = useSelector(selectProfile);
+
+  const handleClick = () => {
+    dispatch(resetProfile());
+    logout();
+  };
+
+  return (
+    <div>
+      <div>@{profile.name}</div>
+      <div role="presentation">
+        <StyledMenuProfile>
+          <Divider className="m-0" />
+          <StyledMenuList>
+            <li className="mt-2">
+              <LinkToProfilePage />
+            </li>
+            <li>
+              <LinkToFavorites />
+            </li>
+            <li className="mb-2">
+              <LinkToDeleted />
+            </li>
+          </StyledMenuList>
+          <Divider className="m-0" />
+        </StyledMenuProfile>
+        <Link to="/" className="d-none d-md-block">
+          <Typography.Link onClick={handleClick}>Выйти</Typography.Link>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PopoverContent;
